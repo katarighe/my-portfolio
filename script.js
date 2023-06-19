78bd026b3d56964aaa68fca9fb76da7673c11149
Newton Ombese <newtonombese1@gmail.com>
2023-06-19 18:07:37 +0300

    Add nav bar functionalities

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Mobile Menu
 const open = document.querySelector('.open-menu');
 const close = document.querySelector('.close-menu');
 const menu = document.querySelector('.mobile-menu');
+const links = document.querySelectorAll('.mobile-links');
 
 open.addEventListener('click', function() {
     menu.style.display = 'block';
@@ -9,6 +9,16 @@ open.addEventListener('click', function() {
 });
 
 close.addEventListener('click', function () {
+    hideMenu();
+});
+
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+        hideMenu();
+    });
+});
+
+function hideMenu() {
     menu.style.display = 'none';
     document.body.style.overflow = 'auto';
-});  
+}
