8e4b4193f290677267cbd27333f147523c7cf2cc
Mohamed Aden Ighe <80690364+katarighe@users.noreply.github.com>
2023-06-19 18:35:20 +0300

    Fix linters error

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const close = document.querySelector('.close-menu');
 const menu = document.querySelector('.mobile-menu');
 const links = document.querySelectorAll('.mobile-links');
 
-open.addEventListener('click', function() {
-    menu.style.display = 'block';
-    document.body.style.overflow = 'hidden';
-});
-
-close.addEventListener('click', function () {
-    hideMenu();
-});
-
-links.forEach(function(link) {
-    link.addEventListener('click', function() {
-        hideMenu();
-    });
-});
-
 function hideMenu() {
-    menu.style.display = 'none';
-    document.body.style.overflow = 'auto';
+  menu.style.display = 'none';
+  document.body.style.overflow = 'auto';
 }
+
+open.addEventListener('click', () => {
+  menu.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+});
+
+close.addEventListener('click', () => {
+  hideMenu();
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    hideMenu();
+  });
+});
